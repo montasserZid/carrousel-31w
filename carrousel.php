5022f05e51bbb48c0e85789f5bd6eefe3e63c490
Montasser <montasser.zid19@gmail.com>
2023-03-08 10:51:36 -0500

    c11 debut carrousel

---
+++ carrousel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Exenstion carrousel permet d'afficher dans une boite modale les images d'une galerie
+ *Package name : Carrousel
+    *Version : 1.0.0
+ */
+
+ /*
+ Plugin Name: Carrousel
+ Author: Montasser Zid
+ Plugin URI: https:github.com/montasserzid/carrousel
+ Description: Extension qui permet d'afficher dans une boite modale les images d'une galerie avec un système de carrousel
---
+++ js/caroussel.js
@@ -0,0 +1,4 @@
+(function() {
+    //debut 
+console.log('carrousel.js');
+})();
