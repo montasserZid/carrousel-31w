f96c4834940f3f5efcdacc7e5e631f55c4504944
Montasser <montasser.zid19@gmail.com>
2023-03-08 11:03:17 -0500

    c11 modification carrousel.php ajout - wp_enque_style

--- carrousel.php
+++ carrousel.php
@@ -10,3 +10,14 @@
  Author: Montasser Zid
  Plugin URI: https:github.com/montasserzid/carrousel
  Description: Extension qui permet d'afficher dans une boite modale les images d'une galerie avec un système de carrousel
+*/
+     wp_enqueue_style(   'em_plugin_carrousel_css',
+                     plugin_dir_url(__FILE__) . "style.css",
+                     array(),
+                     $version_css);
+
+    wp_enqueue_script(  'em_plugin_carrousel_js',
+                    plugin_dir_url(__FILE__) ."js/carrousel.js",
+                    array(),
+                    $version_js,
+                    true);
