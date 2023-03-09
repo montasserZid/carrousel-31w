7f396af2bf315b66b12234eea3c36cf4754d7d0b
Montasser <montasser.zid19@gmail.com>
2023-03-09 15:22:23 -0500

    c12 modification carrousel.php et carrousel.js

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,18 @@
 (function() {
     //debut 
 console.log('carrousel.js');
-})();
+let bouton__ouvrir = document.querySelector('.bouton__ouvrir');
+let elmCarrousel = document.querySelector('.carrousel');
+let elmBouton__x = document.querySelector('.bouton__x');
+console.log(bouton__ouvrir.tagName);
+// on va faire une fonction flechee
+bouton__ouvrir.addEventListener('mouseDown', () => {
+    console.log('boite modal ');
+    elmCarrousel.classList.add('carrousel--ouvert');
+    });
+elmBouton__x.addEventListener('mouseDown', () => {
+    console.log('boite modal ');
+    elmCarrousel.classList.remove('carrousel--ouvert');
+    });
+})()
+//fin
