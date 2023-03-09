868531123ff0099c744d4de4f217a95726dbed82
Montasser <montasser.zid19@gmail.com>
2023-03-09 14:52:00 -0500

    c12 modification carrousel.php (pas fonctionnel pour l'instant)

--- carrousel.php
+++ carrousel.php
@@ -11,6 +11,11 @@
  Plugin URI: https:github.com/montasserzid/carrousel
  Description: Extension qui permet d'afficher dans une boite modale les images d'une galerie avec un système de carrousel
 */
+
+function carrousel_enqueue() {
+      $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+      $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js"); 
+
      wp_enqueue_style(   'em_plugin_carrousel_css',
                      plugin_dir_url(__FILE__) . "style.css",
                      array(),
@@ -21,3 +26,20 @@
                     array(),
                     $version_js,
                     true);
+                  }
+add_action('wp_enqueue_scripts', 'carrousel_enqueue');
+function create_carrousel() 
+{
+
+return '
+<button class="bouton__ouvrir">Ouvrir</button>
+    <div class="carrousel">
+    <button class="bouton__x">X</button>
+    <figure class="carrousel__figure"></figure>
+    <form class="carrousel__form"></form>
+    </div>';
+
+}
+
+add_shortcode('carrousel', 'create_carrousel');
+
